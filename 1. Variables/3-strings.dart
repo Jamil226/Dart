36fc9b6776a8ee32bde808e8652e2e6f16b291e3
Jamil226 <jamil138.amin@gmail.com>
2025-08-27 12:21:51 +0500

    updated-strings

--- 1. Variables/3-strings.dart
+++ 1. Variables/3-strings.dart
@@ -1,30 +1,36 @@
 void main() {
   // Everything in Dart is an object.
   // Strings
-  String myStr = 'Hello World';
+  // String Interpolation
 
-  print('Hello World');
 
-  print(myStr);
+  // String myStr = 'Hello World';
+
+  // print('Hello World');
+
+  // print(myStr);
+
 
   String name = 'Muhammad Jamil';
   print("Hello, ${name}");
 
   //Sub String
-  String firstName = name.substring(0, 8);
-  print(firstName);
+  
+  // String firstName = name.substring(0, 8);
+  // print(firstName);
 
   //Get the index of the String
 
-  int index = name.indexOf(' ');
-  String lastName = name.substring(index).trim();
-  print(lastName);
+  // int index = name.indexOf(' ');
+  // String lastName = name.substring(index).trim();
+  // print(lastName);
 
   //String Length
-  print(name.length);
-  print(name.contains('#'));
-  String fullName = "Muhammad Ahmad Khan";
-  List nameParts = fullName.split(' ');
-  print(nameParts);
-  print(nameParts[0]);
+
+  // print(name.length);
+  // print(name.contains('#'));
+  // String fullName = "Muhammad Ahmad Khan";
+  // List nameParts = fullName.split(' ');
+  // print(nameParts);
+  // print(nameParts[0]);
 }
